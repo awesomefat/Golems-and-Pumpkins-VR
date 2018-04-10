1f006bfa522e76744571ddf010243c660e3d6dcb
awesomefat <awesomefat@gmail.com>
2018-04-10 18:28:35 -0500

    mlitman
    initial commit

---
+++ Goblins and Pumkins/Assets/scripts/pumpkin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pumpkin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Golem")
+        {
+            Destroy(collision.gameObject);
+            Time.timeScale = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if (this.transform.position.y < -3f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
