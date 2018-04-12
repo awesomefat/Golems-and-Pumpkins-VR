7a8003a3c0c63c01f9c019cd94b82b74b81c72ef
mlitman <awesomefat@gmail.com>
2018-04-11 19:02:59 -0500

    mlitman
    added vr stuff

---
+++ Goblins and Pumkins/Assets/scripts/bounceBack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Golem")
+        {
+            print("Collide!!!!!");
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * 1000f, ForceMode.Impulse);
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Goblins and Pumkins/Assets/scripts/golemMovement.cs
+++ Goblins and Pumkins/Assets/scripts/golemMovement.cs
@@ -2,8 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
 
 public class golemMovement : MonoBehaviour {
+    //set these in Unity
+    //public SteamVR_TrackedObject leftController;
+   // public SteamVR_TrackedObject rightController;
+
+    public Hand leftHand;
+    public Hand rightHand;
+
+    //Need to extract the actual controller devices from the tracked objects
+    private SteamVR_Controller.Device deviceL;
+    private SteamVR_Controller.Device deviceR;
 
     private Rigidbody myBody;
     private float moveForce = 10f;
@@ -12,12 +25,31 @@ public class golemMovement : MonoBehaviour {
 	void Awake () 
     {
         this.myBody = this.GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        //this.deviceL = SteamVR_Controller.Input((int)this.leftController.index);
+        //this.deviceR = SteamVR_Controller.Input((int)this.rightController.index);
+    }
+
+
+    // Update is called once per frame
+    void Update()
     {
+        this.deviceL = this.leftHand.controller;
+        this.deviceR = this.rightHand.controller;
+
         float h = Input.GetAxis("Horizontal");
         this.myBody.velocity = new Vector3(-h * this.moveForce, 0f, 0f);
-	}
+
+        if (deviceL.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Vector2 touchpad = (deviceL.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
+            print("Pressing Touchpad " + touchpad.x);
+            h = touchpad.x;
+            this.myBody.velocity = new Vector3(-h * this.moveForce, 0f, 0f);
+        }
+
+        if(deviceR.GetHairTriggerDown())
+        {
+            this.myBody.AddForce(Vector3.up * 2000f, ForceMode.Impulse);
+        }
+    }
 }
